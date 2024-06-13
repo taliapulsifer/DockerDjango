4021d32825ac73fb8039fb1e86b47e5af6a1df1d
Talia Pulsifer <taliaAPulsifer@outlook.com>
2024-06-13 14:16:56 -0500

    Fixed port issue on my end and removed the URL because it was incorrect

--- DockerDjango/DockerDjango/views.py
+++ DockerDjango/DockerDjango/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from main import getContainers, allContainers, containersInfo, removeAllContainers, removeContainer, runContainer
+
+def your_view(request):
+    # Call your Docker function
+    result = your_docker_function()
+    return HttpResponse(result)
