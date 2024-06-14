e53fb87d072138828110ff20fbab272b63f0ad80
Talia Pulsifer <taliaAPulsifer@outlook.com>
2024-06-14 12:33:20 -0500

    Adding examples and adding templates

--- DockerDjango/mainApp/models.py
+++ DockerDjango/mainApp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class TodoItem(models.Model):
+    title = models.CharField(max_length=200)
+    completed = models.BooleanField(default=False)
+
+    #Remember to make migrations every time that you make changes to your models!!
--- DockerDjango/mainApp/views.py
+++ DockerDjango/mainApp/views.py
@@ -4,5 +4,6 @@ from django.shortcuts import render, HttpResponse
 
 # A function that returns some kind of response. Could render html or return httpresponses
 def home(request):
-    return HttpResponse("hello world!")
+    #return HttpResponse("hello world!")
+    return render(request, "home.html")
     #Need to connect through a root or url in the urls.py
