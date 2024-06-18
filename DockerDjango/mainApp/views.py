946f8a2e9120193e854fddfd4dafa7dbd6bdf7c1
Talia Pulsifer <taliaAPulsifer@outlook.com>
2024-06-18 13:12:40 -0500

    Co-authored-by: Gavin Holliday <Gavin-Holliday@outlook.com>

--- DockerDjango/mainApp/views.py
+++ DockerDjango/mainApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse
 from .models import TodoItem
+from . import dockerFunctions
 
 # Create your views here.
 
@@ -12,3 +13,15 @@ def home(request):
 def todos(request):
     items = TodoItem.objects.all()
     return render(request, "todos.html", {"todos": items})
+
+def runContainer(request):
+    if request.method == "POST":
+        image_name = request.POST.get('image_name')
+        if not image_name:
+            return HttpResponse("Error: No image name provided", status=400)
+        try:
+            dockerFunctions.runContainer(imageName=image_name)
+            return HttpResponse("Container started successfully")
+        except Exception as e:
+            return HttpResponse(f"Error: {str(e)}", status=500)
+    return render(request, 'mainApp/home.html')
